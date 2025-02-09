39e63c148f7bb06cfa5c12ad5fd4be497d3c16a4
OnToursSal <ontours.sal@gmail.com>
2025-02-09 21:48:47 +0100

    Add files via upload

--- nieuws.js
+++ nieuws.js
@@ -10,7 +10,7 @@ function getWeather() {
   const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=nl`;
   const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&lang=nl`;
 
-  fetch(currentWeatherUrl)
+  https: fetch(currentWeatherUrl)
     .then((response) => response.json())
     .then((data) => {
       displayWeather(data);
@@ -19,81 +19,37 @@ function getWeather() {
       console.error("Error fetching current weather data:", error);
       alert("Error fetching current weather data. Please try again.");
     });
-
-  // fetch(forecastUrl)
-  //   .then((response) => response.json())
-  //   .then((data) => {
-  //     displayHourlyForecast(data.list);
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error fetching hourly forecast data:", error);
-  //     alert("Error fetching hourly forecast data. Please try again.");
-  //   });
 }
 getWeather();
 setInterval(getWeather, 3600000);
 
 function displayWeather(data) {
+  console.log(data);
   const tempDivInfo = document.getElementById("temp-div");
-  const weatherInfoDiv = document.getElementById("weather-info");
   const weatherIcon = document.getElementById("weather-icon");
-  // const hourlyForecastDiv = document.getElementById("hourly-forecast");
-
-  // Clear previous content
-  weatherInfoDiv.innerHTML = "";
-  // hourlyForecastDiv.innerHTML = "";
-  tempDivInfo.innerHTML = "";
+  const humDivInfo = document.getElementById("hum-div");
+  const cityDivInfo = document.getElementById("city-div");
+  const descriptionInfo = document.getElementById("description-div");
 
   if (data.cod === "404") {
     weatherInfoDiv.innerHTML = `<p>${data.message}</p>`;
   } else {
     const cityName = data.name;
-    const temperature = Math.round(data.main.temp); // Convert to Celsius
+    const temperature = Math.round(data.main.temp);
+    const humidity = data.main.humidity;
     const description = data.weather[0].description;
     const iconCode = data.weather[0].icon;
     const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
 
-    const temperatureHTML = `
-          <p>${temperature}°C</p>
-      `;
-
-    const weatherHtml = `
-          <p>${cityName}</p>
-          <p>${description}</p>
-      `;
-
-    tempDivInfo.innerHTML = temperatureHTML;
-    weatherInfoDiv.innerHTML = weatherHtml;
+    humDivInfo.innerHTML = `hum:${humidity}%`;
+    tempDivInfo.innerHTML = `${temperature}°C`;
+    cityDivInfo.innerHTML = `${cityName}`;
+    descriptionInfo.innerHTML = `${description}`;
     weatherIcon.src = iconUrl;
     weatherIcon.alt = description;
-
     showImage();
   }
 }
-
-// function displayHourlyForecast(hourlyData) {
-//   const hourlyForecastDiv = document.getElementById("hourly-forecast");
-
-//   const next24Hours = hourlyData.slice(0, 8); // Display the next 24 hours (3-hour intervals)
-
-//   next24Hours.forEach((item) => {
-//     const dateTime = new Date(item.dt * 1000); // Convert timestamp to milliseconds
-//     const hour = dateTime.getHours();
-//     const temperature = Math.round(item.main.temp - 273.15); // Convert to Celsius
-//     const iconCode = item.weather[0].icon;
-//     const iconUrl = `https://openweathermap.org/img/wn/${iconCode}.png`;
-
-//     const hourlyItemHtml = `
-//           <div class="hourly-item">
-//               <span>${hour}:00</span>
-//               <img src="${iconUrl}" alt="Hourly Weather Icon">
-//               <span>${temperature + 273}°C</span>
-//           </div>
-//       `;
-
-//     hourlyForecastDiv.innerHTML += hourlyItemHtml;
-//   });
-// }
 
 function showImage() {
   const weatherIcon = document.getElementById("weather-icon");
